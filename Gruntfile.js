8d67a3b80815c45dbe848fe2996bd2323d77f7bc
Hans-Christiaan <hans-christiaan@yoast.com>
2019-05-09 09:43:38 +0200

    Added copy task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,26 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      assets: {
+        expand: true,
+        src: ['source-files/*'],
+        dest: 'dist/assets',
+        flatten: true,
+        filter: 'isFile'
+      },
+      index: {
+        expand: true,
+        src: ['index.html'],
+        dest: 'dist/',
+        flatten: true,
+        filter: 'isFile'
+      }
+    }
   });
 
   // Load tasks here.
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Define aliases here.
   grunt.registerTask('default', 'My default task description', function() {
